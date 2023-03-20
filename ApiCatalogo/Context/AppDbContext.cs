248e838d6dc9d2738ab3c24f44e27098c8cf061d
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2023-03-20 12:23:49 -0300

    mapeamento das entidades

--- ApiCatalogo/Context/AppDbContext.cs
+++ ApiCatalogo/Context/AppDbContext.cs
@@ -12,4 +12,22 @@ public class AppDbContext : DbContext
     {
         
     }
+
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        mb.Entity<Category>().HasKey(x => x.CategoryId);
+        mb.Entity<Category>().Property(x => x.Name).HasMaxLength(100).IsRequired();
+        mb.Entity<Category>().Property(x => x.Description).HasMaxLength(150).IsRequired();
+
+        mb.Entity<Product>().HasKey(x => x.ProductId);
+        mb.Entity<Product>().Property(x => x.Name).HasMaxLength(100).IsRequired();
+        mb.Entity<Product>().Property(x => x.Description).HasMaxLength(150);
+        mb.Entity<Product>().Property(x => x.Image).HasMaxLength(100);
+        mb.Entity<Product>().Property(x => x.Price).HasPrecision(14, 2);
+
+        mb.Entity<Product>()
+            .HasOne(x => x.Category)
+            .WithMany(x => x.Products)
+            .HasForeignKey(x => x.CategoryId);
+    }
 }
