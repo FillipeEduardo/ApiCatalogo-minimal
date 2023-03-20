131efd077bf62f468b925b133499233eded7f2f4
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2023-03-20 12:01:00 -0300

    inicial commit

---
+++ ApiCatalogo/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
